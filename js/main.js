3c0d03c7e7ac35469306a1d98c0121ac09faaf19
rober <japimes@gmail.com>
2017-06-26 18:41:33 +0200

    nueva home gris

--- js/main.js
+++ js/main.js
@@ -4,40 +4,40 @@ var unwidth = $(window).width();
 
 //FUNCIONES
 
-//deja fixed un elemento (elem) en una posicion (top)
-var quieto = function (cosa, top) {
-	var elem_stop = cosa.offset().top - (unheight*top/100); 
-	$(window).scroll(function () {
-		// var elem_stop = elem.offset().top - (unheight*top/100); 
+////deja fixed un elemento (elem) en una posicion (top)
+// var quieto = function (cosa, top) {
+// 	var elem_stop = cosa.offset().top - (unheight*top/100); 
+// 	$(window).scroll(function () {
+// 		// var elem_stop = elem.offset().top - (unheight*top/100); 
 
-	    if ( $(window).scrollTop() > elem_stop ) {
-	  //   	console.log(elem_stop + ", " + elem.offset().top + ", " + unheight + ", " + top);
-			// console.log(elem.offset().top - unheight/2);
+// 	    if ( $(window).scrollTop() > elem_stop ) {
+// 	  //   	console.log(elem_stop + ", " + elem.offset().top + ", " + unheight + ", " + top);
+// 			// console.log(elem.offset().top - unheight/2);
 
-	        cosa.addClass("fixed");
-	        cosa.css("top", top + "vh");
-	    } else {
-	    	cosa.removeClass("fixed");
-	      	cosa.css("top", "initial");
-	    }
-	})
-}
+// 	        cosa.addClass("fixed");
+// 	        cosa.css("top", top + "vh");
+// 	    } else {
+// 	    	cosa.removeClass("fixed");
+// 	      	cosa.css("top", "initial");
+// 	    }
+// 	})
+// }
 
-//deja fixed un elemento (elem) en una posicion (top)
-var parao = function (cosa, top, duration) {
-	var elem_stop = cosa.offset().top - unheight*top/100; 
-	var elem_play = elem_stop + duration; 
+// //deja fixed un elemento (elem) en una posicion (top)
+// var parao = function (cosa, top, duration) {
+// 	var elem_stop = cosa.offset().top - unheight*top/100; 
+// 	var elem_play = elem_stop + duration; 
 
-	$(window).scroll(function () {
-	    if ( (elem_stop < $(window).scrollTop()) && ($(window).scrollTop() < elem_play) ) {
-	        cosa.addClass("fixed");
-	        cosa.css("top", top + "vh");
-	    } else {
-	    	cosa.removeClass("fixed");
-	      	cosa.css("top", "initial");
-	    }
-	})
-}
+// 	$(window).scroll(function () {
+// 	    if ( (elem_stop < $(window).scrollTop()) && ($(window).scrollTop() < elem_play) ) {
+// 	        cosa.addClass("fixed");
+// 	        cosa.css("top", top + "vh");
+// 	    } else {
+// 	    	cosa.removeClass("fixed");
+// 	      	cosa.css("top", "initial");
+// 	    }
+// 	})
+// }
 
 //titulo fixed
 $(window).scroll(function () {
@@ -48,6 +48,28 @@ $(window).scroll(function () {
     }
 })
 
+
+
+//gif del mockup en la home de ucip
+
+//cargar imagenes al ppio
+$(document).ready(function() {
+	for (var i =0; i<145; i++) {
+		$("#mok_1").append("<img class='mockup_in' src='images/nar/nar_" + i + ".jpg' />");	
+	}
+});
+//avanzar gif en scroll
+$(window).scroll(function () {
+
+	var fin = $("#frontmok .mockup").offset().top + $("#frontmok .mockup").height();
+	var i = parseInt($(window).scrollTop() / 5);
+	
+	if ($(window).scrollTop() < fin ) {
+		$("#mok_1").attr("src", "images/nar/nar_"+ i +".jpg");
+	} else {
+		$("#mok_1").attr("src", "images/nar/nar_0.jpg");
+	}  
+})
 
 
 //slider detalles
@@ -101,7 +123,7 @@ $(".puntos .prev").click(function() {
 	} else {
 		$(".puntos .titulo_med").hide();
 	}
-	
+
 	$(".puntos .pos span").text($(".puntos .active").data("car") + "/" + $(".ultimo").data("car"));
 });
 
@@ -124,44 +146,6 @@ $(window).scroll(function () {
 })
 
 
-//fondo amarillo de propuesta UCIP
-
-
-
-// $(window).scroll(function () {
-//     if ( $(window).scrollTop() > propuesta_color) {
-//         $("#uciplus").addClass("fondo_amarillo");
-//     } else {
-//       	$("#uciplus").removeClass("fondo_amarillo");
-//     }
-// })
-
-//gif del mockup en la home de ucip
-$(document).ready(function() {
-
-	for (var i =0; i<145; i++) {
-		$("#mok_1").append("<img class='mockup_in' src='images/nar/nar_" + i + ".jpg' />");	
-	}
-});
-
-
-
-
-
-$(window).scroll(function () {
-	//var frame = 900/232;
-	var fin = $("#frontmok .mockup").offset().top + $("#frontmok .mockup").height();
-
-	var i = parseInt($(window).scrollTop() / 5);
-	
-	if ($(window).scrollTop() < fin ) {
-		$("#mok_1").attr("src", "images/nar/nar_"+ i +".jpg");
-		//$("#mok_1").parent().append("<img class='mockup_in' src='images/nar/nar_" + i + ".jpg' />");
-	} else {
-		$("#mok_1").attr("src", "images/nar/nar_0.jpg");
-	}
-    
-})
 // $(window).scroll(function () {
 // 	var i =  parseInt(($(window).scrollTop() - $(".propuesta_color img").offset().top - unheight*0.3) / 4);
 // 	if (($(window).scrollTop() > $(".propuesta_color img").offset().top - unheight*0.3 ) && ($(window).scrollTop() < $(".propuesta_color img").offset().top + unheight)) {
