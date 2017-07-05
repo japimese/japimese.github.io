aa90e20f5768898e06655ef7b5ef96f474c7c681
rober <japimes@gmail.com>
2017-07-05 13:18:19 +0200

    no message

--- abuelapp/script.js
+++ abuelapp/script.js
@@ -11,18 +11,18 @@ $(document).ready(function() {
 		}, 2000);
 	});
 	
-	if ($(window).width() <= 768) {  
+	// if ($(window).width() <= 768) {  
 
-    	function calcVH() {
-	   		$('.movil').innerHeight( $(window).innerHeight() );
-		}
+ //    	function calcVH() {
+	//    		$('.movil').innerHeight( $(window).innerHeight() );
+	// 	}
 
-		calcVH();
+	// 	calcVH();
 
-		$(window).on('resize orientationchange', function() {
-			calcVH();
-		});
-	}     
+	// 	$(window).on('resize orientationchange', function() {
+	// 		calcVH();
+	// 	});
+	// }     
 	
 
 });
