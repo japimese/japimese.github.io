7dc4c9a0dac82e46ecc11c3256a5e44bab159a16
rober <japimes@gmail.com>
2017-10-09 18:09:03 +0200

    puto scroll policial abuelas landing

---
+++ js/abuelapp.js
@@ -0,0 +1,41 @@
+var unheight = $(window).height();
+var unwidth = $(window).width();
+
+
+
+//iluminar
+
+
+// $(window).scroll(function () { 
+// 	var paso_active = $(".historia .active");
+// 	var ini = paso_active.offset().top - unheight*0.7;
+// 	var fin = ini + unheight*0.5;
+
+// 	if($(window).scrollTop() < $(".historia .pri").offset().top ) {
+// 		$(".historia .pri").addClass("active").siblings().removeClass("active");
+// 	} else if ($(window).scrollTop() > fin)  {
+// 		paso_active.removeClass("active").next().addClass("active");
+
+// 	} else if ($(window).scrollTop() < ini) {
+// 		paso_active.removeClass("active").prev().addClass("active");
+// 	}
+// });
+
+//gifs
+
+// $(window).scroll(function () {
+
+// 	if($(".historia .sec").hasClass("active")) {
+// 		$(".mockup").attr("src", "../images/abuelapp/abp_1.gif");
+// 	} else if ($(".historia .sec").hasClass("active")) {
+// 		$(".mockup").attr("src", "../images/abuelapp/abp_2.gif");
+// 	} else if ($(".historia .ter").hasClass("active")) {
+// 		$(".mockup").attr("src", "../images/abuelapp/abp_3.gif");
+// 	}
+// })
+
+
+
+
+
+
