9ab6b7c8ef6087933bd119e0ea796275527e1ca7
rober <japimes@gmail.com>
2023-05-24 20:43:57 +0200

    aer si funciona cambiando abuelapp_2 por abuelapp

--- abuelapp/precache-manifest.70f5b1148b2c63bf5692362ae7d82a4d.js
+++ abuelapp/precache-manifest.70f5b1148b2c63bf5692362ae7d82a4d.js
@@ -1,138 +1,138 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
     "revision": "999063f1eb40009b821a",
-    "url": "/abuelapp_2/css/app.8eded752.css"
+    "url": "/abuelapp/css/app.8eded752.css"
   },
   {
     "revision": "e059ade26a46c3fb3aff219ae53d709e",
-    "url": "/abuelapp_2/fonts/Changa-Bold.e059ade2.ttf"
+    "url": "/abuelapp/fonts/Changa-Bold.e059ade2.ttf"
   },
   {
     "revision": "c1a282bf2e734550ce86099d96eb0ba1",
-    "url": "/abuelapp_2/fonts/Changa-Light.c1a282bf.ttf"
+    "url": "/abuelapp/fonts/Changa-Light.c1a282bf.ttf"
   },
   {
     "revision": "14f5f301e600a9357882b92b4a189b20",
-    "url": "/abuelapp_2/fonts/Changa-Medium.14f5f301.ttf"
+    "url": "/abuelapp/fonts/Changa-Medium.14f5f301.ttf"
   },
   {
     "revision": "a6aa4fb891b5d078969d1e0c3a7e0135",
-    "url": "/abuelapp_2/fonts/Changa-Regular.a6aa4fb8.ttf"
+    "url": "/abuelapp/fonts/Changa-Regular.a6aa4fb8.ttf"
   },
   {
     "revision": "685eefb3c10874ee3c24c06be82a82bc",
-    "url": "/abuelapp_2/fonts/Changa-SemiBold.685eefb3.ttf"
+    "url": "/abuelapp/fonts/Changa-SemiBold.685eefb3.ttf"
   },
   {
     "revision": "e22062b3188c8199283ef2aa835d4653",
-    "url": "/abuelapp_2/fonts/Roboto-Light.e22062b3.ttf"
+    "url": "/abuelapp/fonts/Roboto-Light.e22062b3.ttf"
   },
   {
     "revision": "99fc0816a09395454061301fefa42bf1",
-    "url": "/abuelapp_2/fonts/Roboto-Medium.99fc0816.ttf"
+    "url": "/abuelapp/fonts/Roboto-Medium.99fc0816.ttf"
   },
   {
     "revision": "54a91b0619ccf9373d525109268219dc",
-    "url": "/abuelapp_2/fonts/Roboto-Regular.54a91b06.ttf"
+    "url": "/abuelapp/fonts/Roboto-Regular.54a91b06.ttf"
   },
   {
     "revision": "f0668b2e3b709435b67345019534ae2c",
-    "url": "/abuelapp_2/img/abu.svg"
+    "url": "/abuelapp/img/abu.svg"
   },
   {
     "revision": "6fe890b2bc2fab119200a15787516bc8",
-    "url": "/abuelapp_2/img/abu_logo_text.svg"
+    "url": "/abuelapp/img/abu_logo_text.svg"
   },
   {
     "revision": "a718ecfe01c52115a5d0619f2ab677c5",
-    "url": "/abuelapp_2/img/brazo_abu.svg"
+    "url": "/abuelapp/img/brazo_abu.svg"
   },
   {
     "revision": "0144317e505f8e4e149fd4aa7f45a8d3",
-    "url": "/abuelapp_2/img/fondo.0144317e.jpg"
+    "url": "/abuelapp/img/fondo.0144317e.jpg"
   },
   {
     "revision": "df4b14410e8eded6ad8e1deaed43c53a",
-    "url": "/abuelapp_2/img/iconos/abrigo.svg"
+    "url": "/abuelapp/img/iconos/abrigo.svg"
   },
   {
     "revision": "72d1681273b6b61af04138a662789997",
-    "url": "/abuelapp_2/img/iconos/botas_lluvia.svg"
+    "url": "/abuelapp/img/iconos/botas_lluvia.svg"
   },
   {
     "revision": "11e4c6a5e2c8a21d961cfe7f6a51d2a3",
-    "url": "/abuelapp_2/img/iconos/botella_agua.svg"
+    "url": "/abuelapp/img/iconos/botella_agua.svg"
   },
   {
     "revision": "20aec406a074dc8b38a51e64c5295208",
-    "url": "/abuelapp_2/img/iconos/gafas_sol.svg"
+    "url": "/abuelapp/img/iconos/gafas_sol.svg"
   },
   {
     "revision": "dc38f5c9c549df735e46ae8dc0044bf0",
-    "url": "/abuelapp_2/img/iconos/gorro_nieve.svg"
+    "url": "/abuelapp/img/iconos/gorro_nieve.svg"
   },
   {
     "revision": "e1b56d77b4963f8fe53d9e375289909f",
-    "url": "/abuelapp_2/img/iconos/guantes_nieve.svg"
+    "url": "/abuelapp/img/iconos/guantes_nieve.svg"
   },
   {
     "revision": "e4b23f9fa392266ef4a007790520eb52",
-    "url": "/abuelapp_2/img/iconos/lazo_viento.svg"
+    "url": "/abuelapp/img/iconos/lazo_viento.svg"
   },
   {
     "revision": "9d636bb9262124bc762fc7d3e8a55944",
-    "url": "/abuelapp_2/img/iconos/paraguas.svg"
+    "url": "/abuelapp/img/iconos/paraguas.svg"
   },
   {
     "revision": "e90ac7f50fe257e95bf7ea1fdd48771e",
-    "url": "/abuelapp_2/img/iconos/paraguas_nieve.svg"
+    "url": "/abuelapp/img/iconos/paraguas_nieve.svg"
   },
   {
     "revision": "57f04846814b351614b513135e131bff",
-    "url": "/abuelapp_2/img/iconos/rebeca.svg"
+    "url": "/abuelapp/img/iconos/rebeca.svg"
   },
   {
     "revision": "a2adcfc7c140bf2803fe6fb891602b95",
-    "url": "/abuelapp_2/img/iconos/sandalias.svg"
+    "url": "/abuelapp/img/iconos/sandalias.svg"
   },
   {
     "revision": "0b80c40f8b9db726359a8ae61cdf310a",
-    "url": "/abuelapp_2/img/iconos/sudadera.svg"
+    "url": "/abuelapp/img/iconos/sudadera.svg"
   },
   {
     "revision": "410dd620b5e7d7f7422df233d8851c0e",
-    "url": "/abuelapp_2/img/mano.410dd620.svg"
+    "url": "/abuelapp/img/mano.410dd620.svg"
   },
   {
     "revision": "410dd620b5e7d7f7422df233d8851c0e",
-    "url": "/abuelapp_2/img/mano.svg"
+    "url": "/abuelapp/img/mano.svg"
   },
   {
     "revision": "546799a2fc42c9293372dcd04590a460",
-    "url": "/abuelapp_2/img/mano_slide.546799a2.svg"
+    "url": "/abuelapp/img/mano_slide.546799a2.svg"
   },
   {
     "revision": "546799a2fc42c9293372dcd04590a460",
-    "url": "/abuelapp_2/img/mano_slide.svg"
+    "url": "/abuelapp/img/mano_slide.svg"
   },
   {
     "revision": "21060d90c240142154a46db174c700a0",
-    "url": "/abuelapp_2/index.html"
+    "url": "/abuelapp/index.html"
   },
   {
     "revision": "999063f1eb40009b821a",
-    "url": "/abuelapp_2/js/app.590cfc6b.js"
+    "url": "/abuelapp/js/app.590cfc6b.js"
   },
   {
     "revision": "dd7b579747cfa4054092",
-    "url": "/abuelapp_2/js/chunk-vendors.17224971.js"
+    "url": "/abuelapp/js/chunk-vendors.17224971.js"
   },
   {
     "revision": "8220517f617500ef3c1b5ab80ac83399",
-    "url": "/abuelapp_2/manifest.json"
+    "url": "/abuelapp/manifest.json"
   },
   {
     "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
-    "url": "/abuelapp_2/robots.txt"
+    "url": "/abuelapp/robots.txt"
   }
 ]);
